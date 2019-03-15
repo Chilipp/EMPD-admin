fd0938ca1314e9b2a65347135b7a480f25fbfb8c
Chilipp <p.sommer@posteo.de>
2019-03-15 12:35:13 +0100

    Include test files in python package

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import os.path as osp
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 
 
 def main():
     setup(name='empd-admin',
           version='1.0',
+          description='Automatic adminstrator of the EMPD',
+          long_description=readme(),
           author='Philipp S. Sommer',
           author_email='philipp.sommer@unil.ch',
           url='https://github.com/EMPD2/EMPD-admin',
           entry_points=dict(
               console_scripts=['empd-admin = empd_admin.__main__:main']),
           packages=find_packages(),
+          package_data={'empd_admin': [
+              osp.join('empd_admin', 'tests', '*.py'),
+              ]},
           include_package_data=True,
           classifiers=[
             'Development Status :: 2 - Pre-Alpha',
