906d80086223cf0fe65e9ee1f45c67ae9ad8af3c
Chilipp <p.sommer@posteo.de>
2019-04-01 21:18:54 +0200

    Access-Control-Allow-Origin * for ViewerHookHandler

--- empd_admin/webapp.py
+++ empd_admin/webapp.py
@@ -145,6 +145,12 @@ class TestHookHandler(tornado.web.RequestHandler):
 
 
 class ViewerHookHandler(tornado.web.RequestHandler):
+
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
     def post(self):
         body = tornado.escape.json_decode(self.request.body)
         try:
