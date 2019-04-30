d7ae2f9d05ea596172e63e83a94ac78dc18fcdc6
Chilipp <p.sommer@posteo.de>
2019-04-30 17:39:00 +0200

    set headers for MainHandler

--- empd_admin/webapp.py
+++ empd_admin/webapp.py
@@ -427,6 +427,11 @@ class VerificationHandler(tornado.web.RequestHandler):
 
 class MainHandler(tornado.web.RequestHandler):
 
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
     def get(self):
         self.write(
             """
