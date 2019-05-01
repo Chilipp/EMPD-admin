70512c23f1d7a55d8168564752a822cf9050bcd0
Chilipp <p.sommer@posteo.de>
2019-05-01 16:00:23 +0200

    Update run-empd-admin-server.sh

--- run-empd-admin-server.sh
+++ run-empd-admin-server.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-set -x
-# start postgres server
-echo "Starting pg server" > ~/starting_pg_server.lock
-echo "Created $HOME/starting_pg_server.lock"
-/bin/bash -c "
-start_pg_server &&
-createdb -U postgres EMPD2 &&
-curl -fsSL https://raw.githubusercontent.com/EMPD2/EMPD-data/master/postgres/EMPD2.sql | psql EMPD2 -U postgres &&
-rm ~/starting_pg_server.lock
-" &
-
-# clone latest version of the EMPD
-git -C /opt/empd-data pull origin master &
 
 # activate conda
 . "/opt/conda/etc/profile.d/conda.sh"
 conda activate empd-admin
+
+set -ex
+# start postgres server
+echo "Starting pg server" > ~/starting_pg_server.lock
+echo "Created $HOME/starting_pg_server.lock"
+bash -c "
+start_pg_server &&
+createdb -U postgres EMPD2 &&
+curl -fsSL https://raw.githubusercontent.com/EMPD2/EMPD-data/master/postgres/EMPD2.sql | psql EMPD2 -U postgres > /dev/null &&
+rm ~/starting_pg_server.lock
+" &
+
+# clone latest version of the EMPD in the herokuapp
+bash -c 'if [[ $ONHEROKU ]]; then git -C /opt/empd-data pull origin master; fi' &
 
 git config --global user.name "EMPD-admin"
 
